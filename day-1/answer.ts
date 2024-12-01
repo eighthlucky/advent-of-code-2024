89315c7a63745d2d5013c238b4aea5ee21bb0292
Peter Tran <24271588+eighthlucky@users.noreply.github.com>
2024-12-02 00:15:59 +1000

    feat(day-1): add answer for part 2

--- day-1/answer.ts
+++ day-1/answer.ts
@@ -20,6 +20,15 @@ if (listA.length !== listB.length) {
   throw new Error('unequal list length');
 }
 
-// Part 1 - sum of distances between lists
+// Part 1 - sum of distances between list values
 const distances = listA.map((a, i) => Math.abs(a - listB[i]));
 console.log('total distance:', distances.reduce((acc, v) => acc + v, 0));
+
+// Part 2 - similarity score
+// List A * no. of appearances in list B
+const appearancesByNumber = Object.groupBy(listB, (v) => v);
+const scores = listA.map((v) => {
+  const frequency = appearancesByNumber[v]?.length ?? 0;
+  return v * frequency;
+});
+console.log('total similarity score:', scores.reduce((acc, v) => acc + v, 0));
