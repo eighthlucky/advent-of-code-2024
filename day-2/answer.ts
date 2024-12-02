55322b81a2868b0b55139be2f6fabdff537ab9e6
Peter Tran <24271588+eighthlucky@users.noreply.github.com>
2024-12-03 01:26:18 +1000

    feat(day-2): incorrect attempt of part-2 using search
    Attempt to move through levels of report, branching when we come
    across an invalid sequence of numbers.

--- day-2/answer.ts
+++ day-2/answer.ts
@@ -2,6 +2,12 @@
 console.log('---Day 2---');
 
 const text = Deno.readTextFileSync(`${import.meta.dirname}/input.txt`);
+// const text = `7 6 4 2 1
+// 1 2 7 8 9
+// 9 7 6 2 1
+// 1 3 2 4 5
+// 8 6 4 4 1
+// 1 3 6 7 9`;
 const reports = text.split('\n')
   .map((line) =>
     line
@@ -43,48 +49,42 @@ const reports = text.split('\n')
 // Part 2 - Count valid reports, tolerating a single error
 {
   let validCount = 0;
+  const getCost = (a: number | undefined, b: number | undefined, c: number) => {
+    if (b === undefined) return 0;
+    const diff = c - b;
+    if (!diff) return 1;
 
-  for (const _report of reports) {
-    const errorIndexes = new Set<number>();
-    const reversedErrorIndexes = new Set<number>();
+    const dist = Math.abs(diff);
+    if (dist < 1 || dist > 3) return 1;
+    if (a !== undefined && Math.sign(diff) !== Math.sign(b - a)) return 1;
+  };
 
-    for (const report of [_report, _report.toReversed()]) {
-      let [prevLevel, ...remaining] = report;
-      const gradient = Math.sign(remaining[0] - prevLevel);
+  for (const report of reports) {
+    const queue = [[-1, 0]];
+    const path = [];
 
-      let prevIndex = 0;
-      for (const level of remaining) {
-        const index = ++prevIndex;
+    while (queue.length) {
+      const [prev, index] = queue.pop() ?? [];
+      if (index >= report.length) break;
 
-        const diff = level - prevLevel;
-        const absoluteDiff = Math.abs(diff);
-        const _gradient = Math.sign(diff);
+      const cost = getCost(
+        report[index - 2],
+        report[prev],
+        report[index],
+      );
 
-        // Valid reports must be in ASC or DESC order, and the difference
-        // between levels must be between 1 and 3 (incl.)
-        const isInvalid = _gradient !== gradient ||
-          absoluteDiff < 1 ||
-          absoluteDiff > 3;
-
-        if (isInvalid) {
-          const isReversed = report !== _report;
-          const _errorIndexes = isReversed
-            ? reversedErrorIndexes
-            : errorIndexes;
-          _errorIndexes.add(
-            isReversed ? (report.length - 1) - index : index,
-          );
-        } else {
-          prevLevel = level;
-        }
+      const next = index + 1;
+      if (cost) {
+        queue.push([prev, next]);
+        continue;
       }
+
+      queue.push([index, next]);
+      if (index >= 0) path.push(report[index]);
     }
 
-    const commonErrorIndexes = errorIndexes.intersection(reversedErrorIndexes);
-    const isSafe = !errorIndexes.union(commonErrorIndexes).size ||
-      errorIndexes.intersection(reversedErrorIndexes).size === 1;
-
-    if (isSafe) validCount++;
+    const isValid = path.length >= report.length - 1;
+    if (isValid) validCount++;
   }
 
   console.log('valid reports with tolerance 1:', validCount);
