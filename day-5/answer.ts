11a9bd82ae5ef69deda5d39acf17fbd8dda92598
Peter Tran <24271588+eighthlucky@users.noreply.github.com>
2024-12-05 21:47:49 +1000

    feat(day-5): attempt part-2
    Tweak answer from part 1 to use lookup map for sorting.

--- day-5/answer.ts
+++ day-5/answer.ts
@@ -51,3 +51,65 @@ const lines = text.split(/\n+/);
 
   console.log('total sum of middle page numbers of valid lines:', sum);
 }
+
+// Part 2 - Print middle number of incorrectly ordered lines after correctly
+// sorting them
+{
+  const rules = lines.values()
+    .filter((line) => line[2] === '|')
+    .map((rule) => rule.split('|'));
+
+  // Group rules by start
+  const lookup = new Map(
+    Object.entries(
+      Object.groupBy(
+        rules,
+        (rule) => rule[0],
+      ),
+    ).map(([k, rules]) => [
+      k,
+      new Set(rules?.map((rule) => rule[1]) ?? []),
+    ]),
+  );
+
+  const sum = lines.values()
+    // Page numbers are delimited by ','
+    .filter((line) => line[2] === ',')
+    .map((line) => line.split(','))
+    // Find lines with page numbers in incorrect order
+    .filter((pageNumbers) => {
+      for (let i = 0; ++i < pageNumbers.length;) {
+        const a = pageNumbers[i - 1];
+        const b = pageNumbers[i];
+
+        // If it's not in the wrong order, it's correct
+        const isValid = !lookup.get(b)?.has(a);
+        if (!isValid) return true;
+      }
+
+      return false;
+    })
+    // Sort them into correct order
+    .map((pageNumbers) =>
+      pageNumbers.toSorted(
+        (a, b) => {
+          if (lookup.get(a)?.has(b)) return -1;
+          if (lookup.get(b)?.has(a)) return 1;
+          return 0;
+        },
+      )
+    )
+    // Sum of middle page numbers
+    .reduce(
+      (acc, pageNumbers) => {
+        const midPoint = Math.floor(pageNumbers.length / 2);
+        return acc + Number.parseInt(pageNumbers[midPoint], 10);
+      },
+      0,
+    );
+
+  console.log(
+    'total sum of middle page numbers of invalid lines after sorting:',
+    sum,
+  );
+}
