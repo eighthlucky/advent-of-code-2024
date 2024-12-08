5c2119d2364b190d81c37ced1fe05bda84c204ea
Peter Tran <24271588+eighthlucky@users.noreply.github.com>
2024-12-08 13:13:38 +1000

    feat(day-7): add attempt for part 1
    Reverse operand order and apply operations in reverse
    until we know the equation is valid.
    We do it in reverse as that lets us return early for
    invalid equations.

---
+++ day-7/answer.ts
@@ -0,0 +1,71 @@
+// setup
+console.log('---Day 7---');
+
+const text = Deno.readTextFileSync(new URL('./input.txt', import.meta.url));
+const equations = text.split('\n')
+  .map((equation) => {
+    const [_result, _operands] = equation.split(':');
+
+    const result = Number.parseInt(_result);
+    const operands = _operands
+      .trim()
+      .split(' ')
+      .map((v) => Number.parseInt(v));
+
+    return [result, ...operands];
+  });
+
+// Part 1 - Figure out results of valid operations
+// Valid operations calculate results by either adding/multiplying operands
+// in left-to-right order
+{
+  // Recursively check whether operands add/multiply to a given result
+  // Needs operands to be reversed
+  const isValidEquation = (
+    result: number,
+    operands: ReadonlyArray<number>,
+  ): boolean => {
+    if (!result) return true;
+    if (!operands.length) return false;
+
+    const [operand, ...remaining] = operands;
+
+    const interimMultiply = result / operand;
+    const interimSum = result - operand;
+
+    if (remaining.length === 1) {
+      // Case: a + b = result
+      // Therefore a = result - b
+      return interimSum === remaining[0] ||
+        // Case: a * b = result
+        // Therefore a = result / b
+        interimMultiply === remaining[0];
+    }
+
+    // There are only integers in the list of operands
+    // so we can skip this isn't the case
+    const canContinueCheckingMultiply = Number.isSafeInteger(interimMultiply);
+
+    // We can still check if the remainder is positive
+    // and there are operands left to subtract
+    const canContinueCheckingSum = interimSum > 0;
+
+    return (canContinueCheckingMultiply &&
+      isValidEquation(interimMultiply, remaining)) ||
+      (canContinueCheckingSum && isValidEquation(interimSum, remaining));
+  };
+
+  const sumValidResults = equations
+    .map(([result, ...operands]) =>
+      isValidEquation(
+          result,
+          // Reverse operands so we can check them right-to-left
+          operands.toReversed(),
+        )
+        ? result
+        : 0
+    )
+    .reduce((acc, v) => acc + v, 0);
+
+  console.log('sum of results for valid equations:', sumValidResults);
+}
